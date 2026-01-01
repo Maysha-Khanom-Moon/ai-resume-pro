9a7a4e32f4f6f12fa5ab42d91d3df92d402cfa51
Maysha-Khanom-Moon <maysha412@gmail.com>
2026-01-02 02:58:03 +0600

    authjs authentication added by google & github provider

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import Link from "next/link"
+
+export default function Register() {
+    return (
+      <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border-[#121212] dark:bg-black">
+        <h2>Welcome to AIResumePro</h2>
+        <p>Pleaes provide all the necessary information</p>
+      </div>
+    )
+}
